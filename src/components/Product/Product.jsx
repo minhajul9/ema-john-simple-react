acc2119073aa000362ba61e4b7ff57a8f3708fc2
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2023-03-29 00:33:02 +0600

    final

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import './Product.css'
+import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const Product = (props) => {
     const { img, price, seller, name, ratings } = props.product;
+
+    const handleAddToCart = props.handleAddToCart;
+
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -12,10 +17,11 @@ const Product = (props) => {
                 <p>Manufacturer: {seller}</p>
                 <p>Rating: {ratings} Stars</p>
             </div>
-            <button className='btn-cart'>Add to Cart</button>
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>Add to Cart 
+            <FontAwesomeIcon icon={faShoppingCart} />
+            </button>
         </div>
     );
 };
-import './Product.css'
 
 export default Product; <h3>This is product</h3>
