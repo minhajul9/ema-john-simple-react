c9cc8c33422986a43cf35294c3b5a8e2fd88740f
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2023-04-30 14:23:45 +0600

    sign up and login page added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import './Login.css'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <h2>Login coming soon</h2>
+        <div className='form-container'>
+            <h2 className='form-title'>Login</h2>
+
+            <form>
+                <div className="form-control">
+                    <label htmlFor="">Email</label>
+                    <input type="email" name="email" id="" required placeholder='Enter your Email' />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="">Password</label>
+                    <input type="password" name="password" id="" required placeholder='Enter your password' />
+                </div>
+                <input type="submit" value="Login" className='btn-submit' />
+                <p className='new-acc'>New to Ema-john? <Link to='/signup' className='create-new'>Create New Account</Link></p>
+            </form>
         </div>
     );
 };
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SignUp.css'
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+
+    const [error, setError] = useState('');
+
+    const handleSignUp = event => {
+        event.preventDefault();
+
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const confirm = form.confirm.value;
+        console.log(email, password, confirm);
+
+        if(password !== confirm){
+            setError("Password does not match");
+            return
+        }
+        else if(password.length < 6){
+            setError('Password must be at least 6 character')
+        }
+    }
+
     return (
-        <div>
-            SignUp
+        <div className='form-container'>
+            <h2 className='form-title'>Sign Up</h2>
+
+            <form onSubmit={handleSignUp}>
+                <div className="form-control">
+                    <label htmlFor="">Email</label>
+                    <input type="email" name="email" id="email" required placeholder='Enter your Email' />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="">Password</label>
+                    <input type="password" name="password" id="password" required placeholder='Enter your password' />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="">Password</label>
+                    <input type="password" name="confirm" id="confirm" required placeholder='Confirm password' />
+                </div>
+                <p className='error-text'>{error}</p>
+                <input type="submit" value="Sign Up" className='btn-submit' />
+
+                <p className='new-acc'>Already have an account? <Link to='/login' className='create-new'>Login</Link></p>
+            </form>
         </div>
     );
 };
